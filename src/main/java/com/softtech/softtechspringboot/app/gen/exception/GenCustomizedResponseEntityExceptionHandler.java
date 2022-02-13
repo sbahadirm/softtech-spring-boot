eede1b3d1de08b4a1ea03ed1abaef1cec0577840
sbahadirm <sbahadirm@gmail.com>
2022-02-13 10:49:56 +0300

    add GenCustomizedResponseEntityExceptionHandler

---
+++ src/main/java/com/softtech/softtechspringboot/app/gen/exception/GenCustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,59 @@
+package com.softtech.softtechspringboot.app.gen.exception;
+
+import com.softtech.softtechspringboot.learning.exception.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+/**
+ * @author Bahadır Memiş
+ * @since 1.0.0
+ */
+@RestController
+@ControllerAdvice
+public class GenCustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest webRequest){
+
+        Date errorDate = new Date();
+        String message = ex.getMessage();
+        String description = webRequest.getDescription(false);
+
+        GenExceptionResponse genExceptionResponse = new GenExceptionResponse(errorDate, message, description);
+
+        return new ResponseEntity<>(genExceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleAllUserNotFoundException(UserNotFoundException ex, WebRequest webRequest){
+
+        Date errorDate = new Date();
+        String message = ex.getMessage();
+        String description = webRequest.getDescription(false);
+
+        GenExceptionResponse genExceptionResponse = new GenExceptionResponse(errorDate, message, description);
+
+        return new ResponseEntity<>(genExceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Date errorDate = new Date();
+        String message = "Validation failed!";
+        String description = ex.getBindingResult().toString();
+
+        GenExceptionResponse genExceptionResponse = new GenExceptionResponse(errorDate, message, description);
+
+        return new ResponseEntity<>(genExceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+}
